7e673f16bd4450600c2c7f81aac4ab51590d6f0a
dungpt3003 <dungpt3003@gmail.com>
2017-08-30 16:59:51 +0700

    feat: Complete default Q-learning agent and answer Question 6

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -8,7 +8,7 @@ class LearningAgent(Agent):
     """ An agent that learns to drive in the Smartcab world.
         This is the object you will be modifying. """
 
-    def __init__(self, env, learning=False, epsilon=1.0, alpha=0.5):
+    def __init__(self, env, learning=True, epsilon=1.0, alpha=0.5):
         super(LearningAgent, self).__init__(env)     # Set the agent in the evironment
         self.planner = RoutePlanner(self.env, self)  # Create a route planner
         self.valid_actions = self.env.valid_actions  # The set of valid actions
@@ -69,7 +69,7 @@ class LearningAgent(Agent):
         # With the hand-engineered features, this learning process gets entirely negated.
 
         # Set 'state' as a tuple of relevant data for the agent
-        state = (waypoint, inputs['left'], inputs['oncoming'], deadline)
+        state = (waypoint, inputs['light'], inputs['left'], inputs['oncoming'])
 
         return state
 
